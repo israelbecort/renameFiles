a21dcacbb4d01d2fd507bd69e28d71c2778055da
israelbecort <israel.becerra97@gmail.com>
2023-08-07 18:30:29 +0200

    añadida pregunta para numero de episodio

--- renameFiles.py
+++ renameFiles.py
@@ -15,7 +15,11 @@ else:
     print(listaFicheros)
 
     for i in range(len(listaFicheros)):
+        print(listaFicheros[i])
         numeroEpisodio = (i+1)
+        isEp = input('¿es el episodio ' + str(numeroEpisodio) + '? (y/n): ')
+        if(isEp != "" and isEp != "y"):
+            numeroEpisodio = input('¿Qué episodio es?: ')
         oldName=listaFicheros[i]
         extension=oldName.split(".")
         extension=extension[len(extension)-1]
