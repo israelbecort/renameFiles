43d6a1b1cdf7921707d269018c6d99acf939c969
israelbecort <israel.becerra97@gmail.com>
2023-08-08 21:17:04 +0200

    modificado script series

--- renameFiles.py
+++ renameFiles.py
@@ -5,24 +5,24 @@ print(SourceFolder)
 nombreSerie = input('¿Cuál es el nombre de la serie?: ')
 TemporadaSerie = input('¿Cuál es la temporada de la serie? (01, 02, 03, etc): ')
 
-subcarpetaName = input('Nombre de la subcarpeta: ') +"\\"
+listaCarpetas = os.listdir(SourceFolder)
+os.mkdir(SourceFolder + nombreSerie)
+for i in range(len(listaCarpetas)):
+    if(listaCarpetas[i] == "Series" or listaCarpetas[i] == "Peliculas" or listaCarpetas[i] == "renameFiles.py"):
+        print("no se toca la carpeta " + listaCarpetas[i])
+    else:
+        subcarpetaName = listaCarpetas[i] + "\\"
+        folder = SourceFolder + subcarpetaName
+        listaFicheros = os.listdir(folder)
 
-if(subcarpetaName=="\\"):
-    print("Los ficheros deben estar en una subcarpeta")
-else:
-    folder = SourceFolder + subcarpetaName
-    listaFicheros = os.listdir(folder)
-
-    for i in range(len(listaFicheros)):
-        print(listaFicheros[i])
-        numeroEpisodio = (i+1)
-        isEp = input('¿es el episodio ' + str(numeroEpisodio) + '? (y/n): ')
-        if(isEp != "" and isEp != "y"):
-            numeroEpisodio = input('¿Qué episodio es?: ')
-        oldName=listaFicheros[i]
-        extension=oldName.split(".")
-        extension=extension[len(extension)-1]
-        newName=nombreSerie + " S" + TemporadaSerie + "E" + str(numeroEpisodio) + "." + extension
-        os.rename(folder + oldName, folder + newName)
-        
-shutil.move(folder, SourceFolder+"Series")
+        for i in range(len(listaFicheros)):
+            print(listaFicheros[i])
+            numeroEpisodio = input('¿Qué episodio es este?: ')
+            oldName=listaFicheros[i]
+            extension=oldName.split(".")
+            extension=extension[len(extension)-1]
+            newName=nombreSerie + " S" + TemporadaSerie + "E" + str(numeroEpisodio) + "." + extension
+            os.rename(folder + oldName, folder + newName)
+            shutil.move(folder + newName, SourceFolder + nombreSerie)
+        os.rmdir(folder)
+shutil.move(SourceFolder + nombreSerie, SourceFolder+"Series")
