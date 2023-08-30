d1e6e4ab8e47f909dddf5cda88ea8071376956ed
israelbecort <israel.becerra97@gmail.com>
2023-08-30 19:50:13 +0200

    añadido codigo para episodios duplicados y distintas temporadas de la misma serie

--- renameFiles.py
+++ renameFiles.py
@@ -1,12 +1,23 @@
 import os
 import shutil
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 SourceFolder = "G:\Mi unidad\Plex\\"
 print(SourceFolder)
 nombreSerie = input('¿Cuál es el nombre de la serie?: ')
 TemporadaSerie = input('¿Cuál es la temporada de la serie? (01, 02, 03, etc): ')
 
-listaCarpetas = os.listdir(SourceFolder)
-os.mkdir(SourceFolder + nombreSerie)
+listaCarpetas = os.listdir(SourceFolder) #Listado de carpetas dentro de SourceFolder
+os.mkdir(SourceFolder + nombreSerie) #Creación de carpeta con el nombre de la serie
 for i in range(len(listaCarpetas)):
     if(listaCarpetas[i] == "Series" or listaCarpetas[i] == "Peliculas" or listaCarpetas[i] == "renameFiles.py"):
         print("no se toca la carpeta " + listaCarpetas[i])
@@ -16,6 +27,7 @@ for i in range(len(listaCarpetas)):
         listaFicheros = os.listdir(folder)
 
         for i in range(len(listaFicheros)):
+            
             print(listaFicheros[i])
             numeroEpisodio = input('¿Qué episodio es este?: ')
             oldName=listaFicheros[i]
@@ -23,6 +35,22 @@ for i in range(len(listaCarpetas)):
             extension=extension[len(extension)-1]
             newName=nombreSerie + " S" + TemporadaSerie + "E" + str(numeroEpisodio) + "." + extension
             os.rename(folder + oldName, folder + newName)
-            shutil.move(folder + newName, SourceFolder + nombreSerie)
+            try:
+                shutil.move(folder + newName, SourceFolder + nombreSerie)
+            except:
+                os.rename(folder + newName, folder + oldName)
+                print(listaFicheros[i])
+                numeroEpisodio = input(bcolors.WARNING + 'WARNING: El número de episodio que has introducido ya existe, ¿Qué episodio es?: '+bcolors.ENDC)
+                newName=nombreSerie + " S" + TemporadaSerie + "E" + str(numeroEpisodio) + "." + extension
+                os.rename(folder + oldName, folder + newName)
+                shutil.move(folder + newName, SourceFolder + nombreSerie)
         os.rmdir(folder)
-shutil.move(SourceFolder + nombreSerie, SourceFolder+"Series")
+listaCarpetasSeries = os.listdir(SourceFolder+"Series")
+if (nombreSerie in listaCarpetasSeries): #Si la carpeta ya existe en Series
+    listaFicheros = os.listdir(SourceFolder + nombreSerie)
+    for i in range(len(listaFicheros)):
+        shutil.move(SourceFolder + nombreSerie + "\\" + listaFicheros[i], SourceFolder+"Series\\"+nombreSerie)
+    os.rmdir(SourceFolder + nombreSerie)
+else:
+    shutil.move(SourceFolder + nombreSerie, SourceFolder+"Series")
+input(bcolors.OKGREEN+'Proceso finalizado exitosamente. Presiona ENTER para salir'+bcolors.ENDC)
